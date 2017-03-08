cc1960fc6fc017467d89bc3d4197df6ed33714f1
AlexAbril <alexabrilbn@gmail.com>
2017-03-08 18:55:24 +0100

    Creating Hello World

--- src/Hello.java
+++ src/Hello.java
@@ -5,5 +5,34 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+
+        int a;
+        a = 5;
+        int b = a++;
+
+
+        int c = 11 % 4;
+
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+        System.out.println("c=" + c);
+
+
+        double d1 = 45.5;
+        double d2 = 5.2;
+        double d3 = d1 / d2;
+
+        System.out.println(d3);
+
+        Float f1 = new Double(d3).floatValue();
+        f1 = new Integer(b).floatValue();
+
+        System.out.println(f1);
+
+        Integer il = f1.intValue();
+
+        System.out.println(il);
+
     }
 }
